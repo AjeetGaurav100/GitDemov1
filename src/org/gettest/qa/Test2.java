8149174f3abdac99f7d849668c46878f10305f0a
317667 <317667@5CD2188LZT.SerWizSol.com>
2024-05-18 19:23:33 +0530

    Created Validation in the Test2 class.

--- src/org/gettest/qa/Test2.java
+++ src/org/gettest/qa/Test2.java
@@ -4,6 +4,12 @@ public class Test2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Git Code from Sub branch");
+		int age=23;
+		
+		
+		if(age>=18) {
+			System.out.println("you can vote");
+		}
 	}
 
 }
